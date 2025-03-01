6eb5804567d713b4e5152eb660cbf9993f6094f5
Jakub <devcobbier@gmail.com>
2025-03-01 13:27:36 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'cineseek';
-}
+export class AppComponent {}
